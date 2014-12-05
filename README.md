69df87f83a089620e1ce5399837c259ab47ddf35
Conner Hewitt <connerh87@gmail.com>
2014-12-05 13:35:01 -0600

    Files added

--- README.md
+++ README.md
@@ -1,4 +1,76 @@
 website-mobile-preview
 ======================
 
-Display websites within an image of a mobile phone.
+# Features
+
+* Currently supports iPhone 6.
+* 3 different perspectives - front, left, and right facing.
+* Choose from the black, white, and gold versions.
+* Easy jQuery-like initialization.
+* Scrollbars modified for webkit to resemble track and thumb of iPhone 6
+* Coded in vanilla Javascript, no library dependencies
+
+# Usage
+
+Place the script and images on your server.
+
+Include the script.
+
+```html
+<script type="text/javascript" src="biomp.js"></script>
+```
+
+Create a div element, which will contain the preview.
+
+```html
+<div id="preview"></div>
+```
+
+Initialize a new bioMp object using the div.
+
+*Vanilla JavaScript*
+
+```javascript
+bioMp(document.getElementById('preview'));
+```
+
+*jQuery*
+
+```javascript
+bioMp($('#preview'));
+```
+
+You must include options such as an image and viewing perspective for the preview to show correctly.  This is shown in the next section.
+
+# Options
+
+All options must be added to the init function as an object.
+
+Name | Type | Default | Description
+-----|------|---------|------------
+**url** | string | blank | URL of the web page to preview.
+**view** | string | front | The perspective of the phone.  Possible values are **front**, **left**, and **right**.
+**image** | string | blank | URL of the image to use for the phone. A total of 9 images are included with this script.
+**scale** | float | 1 | Resize the phone preview by a multiplier value. For instance, a value of 1 is full size, while 0.5 would half the size. If this option is set, both width and height options are ignored.
+**width** | integer | 428 | The width of the phone in pixels. If this option is set, the height option is automatically set to proportion. If scale is not set, it will be automatically set to proportion as well.
+**height** | integer | 889 | The height of the phone in pixels. If this option is set, both the scale and width options are ignored and automatically set to proportion.
+
+For example, to create a left facing preview for http://beeker.io on the gold iPhone, you would initiate the preview div in the usage example with these options.
+
+```javascript
+bioMp(document.getElementById('preview'), {
+    url: 'http://beeker.io',
+    view: 'left',
+    image: 'images/iphone6_side_left_gold.png'
+});
+```
+
+# Credits
+
+Thank you to JustD for the images of the iPhone 6.  You can find his work [here on Behanced](https://www.behance.net/justd).
+
+# License
+
+MIT license, feel free to use however you wish!
+
+Created by [beeker.io](http://beeker.io/display-website-in-iphone-html-css-javascript)
---
+++ biomp.js
@@ -0,0 +1,100 @@
+window.bioMp = function(el, options) {
+	// Private
+	var front = {width: 428, height: 889, ratio: (428 / 889)},
+	    side = {width: 302, height: 889, ratio: (302 / 889)};
+	
+	// Options
+	var options = options || {},
+	    url = setOption(options.url, ''),
+	    view = setOption(options.view, 'front'),
+	    image = setOption(options.image, ''),
+	    scale = setOption(options.scale, 0),
+	    width = setOption(options.width, 0),
+	    height = setOption(options.height, 0);
+	
+	function setOption(option, _default) {
+		return (typeof option === 'undefined') ? _default : option;
+	}
+	
+	function setSizes() {
+		var selView = (view == 'front') ? front : side;
+		
+		if(scale != 0) {
+			width = (selView.width * scale);
+			height = (selView.height * scale);
+			return;
+		}
+		
+		if(width != 0) {
+			scale = (width / selView.width);
+			height = (width / selView.ratio);
+			return;
+		}
+		
+		if(height != 0) {
+			scale = (height / selView.height);
+			width = (height * selView.ratio);
+			return;
+		}
+		
+		scale = 1;
+		width = selView.width;
+		height = selView.height;
+	}
+	
+	function create() {
+		// Set view dimensions
+		var selView = (view == 'front') ? front : side;
+
+		// Set scrollbar CSS		
+		var css = document.createTextNode(
+			'.bio-mp-screen::-webkit-scrollbar {width: 6px; height: 6px;}' +
+			'.bio-mp-screen::-webkit-scrollbar-track {background: none; -webkit-border-radius: 10px; border-radius: 10px;}' +
+			'.bio-mp-screen::-webkit-scrollbar-thumb {-webkit-border-radius: 10px; border-radius: 10px; background: #777;}' +
+			'.bio-mp-screen::-webkit-scrollbar-thumb:window-inactive {background: #777;}'
+		);
+		
+		var style = document.createElement('style');
+		style.type = 'text/css';
+		style.appendChild(css);
+		
+		// Add phone div
+		var phone = document.createElement('div');
+		el.appendChild(phone);
+
+		// Add phone image
+		var phoneImage = document.createElement('img');
+		phoneImage.src = image;
+		phone.appendChild(phoneImage);
+		
+		// Add iframe
+		var iframe = document.createElement('iframe');
+		iframe.src = url;
+		iframe.className = 'bio-mp-screen';
+		phone.appendChild(iframe);
+
+		// Add CSS
+		var wrapCss = 'width: ' + width + 'px; height: ' + height + 'px; overflow: hidden; transform-origin: 0 0 0;';
+		var phoneCss = 'position: relative; top: 0px; left: 0px; width: ' + selView.width + 'px; height: ' + selView.height + 'px; background: none; transform-origin: 0 0 0; transform: scale(' + scale + ');';
+		var phoneImageCss = 'position: absolute; top: 0; left: 0; width: 100%; height: 100%;';
+
+		if(view == 'front')
+			var screenCss = 'position: absolute; top: 109px; left: 26.5px; width: 375px; height: 669px; border: 0;';
+		else if(view == 'left')
+			var screenCss = 'position: absolute; top: 135px; left: -60px; width: 375px; height: 669px; border: 0; transform: matrix3d(0.682, -0.160, 0, -0.000380, 0, 0.972, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);';
+		else
+			var screenCss = 'position: absolute; top: 135px; left: -13px; width: 375px; height: 669px; border: 0; transform: matrix3d(0.682, 0.160, 0, 0.000380, 0, 0.972, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);';
+
+		el.style.cssText += wrapCss;
+		phone.style.cssText = phoneCss;
+		phoneImage.style.cssText = phoneImageCss;
+		iframe.style.cssText = screenCss;
+		
+		// Add scrollbar CSS above the wrap
+		el.parentNode.insertBefore(style, el);
+	}
+	
+	// Init
+	setSizes();
+	create();
+}
